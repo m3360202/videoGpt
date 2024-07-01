0f2074ae40a9c6e5a391f4ffa68bcab699f96e3d
justgoodluck <403028240@qq.com>
2024-07-01 21:55:19 +0800

    hidden vid

--- src/components/SiteVideoList.jsx
+++ src/components/SiteVideoList.jsx
@@ -48,7 +48,7 @@ export default function SiteVideoList({ data }) {
           <TableCell className="font-medium text-sm">{video?.videox?.title}</TableCell>
           <TableCell className="font-medium text-sm">{video.name}</TableCell>
           <TableCell><img className="w-12" src={video.image} /></TableCell>
-          <TableCell>{video.vid}</TableCell>
+          {/* <TableCell>{video.vid}</TableCell> */}
           <TableCell>{video.subtitles ? (
             <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">已上传</span>
           ) : (
