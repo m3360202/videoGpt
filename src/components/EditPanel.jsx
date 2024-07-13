ab42444eb35bf66d1516dcc4b4468f1f8d0bb48f
kiddyu <58631254@qq.com>
2024-07-14 03:04:26 +0800

    add subtitles loading

--- src/components/EditPanel.jsx
+++ src/components/EditPanel.jsx
@@ -20,9 +20,9 @@ export function EditPanel({ task, file }) {
     if (file.data.ocr_introduce_url && file.data.ocr_subtitle_url && !file.data.ocr_vtt) {
       async function main() {
         console.log('', [file.data.ocr_introduce_url, file.data.ocr_subtitle_url])
-        file.data.ocr_vtt = await getVideoMergedCues([file.data.ocr_introduce_url, file.data.ocr_subtitle_url]);
-        console.log('file.data.ocr_vtt', file.data.ocr_vtt)
-        updateTaskVTT(task.id, file.id, 'ocr_vtt', file.data.ocr_vtt);
+        const ocr_vtt = await getVideoMergedCues([file.data.ocr_introduce_url, file.data.ocr_subtitle_url]);
+        console.log('file.data.ocr_vtt', ocr_vtt)
+        updateTaskVTT(task.id, file.id, 'ocr_vtt', ocr_vtt);
         // updateVideoVTT();
       }
       main();
@@ -146,6 +146,12 @@ export function EditPanel({ task, file }) {
             </div>)}
           </TabsContent> */}
           <TabsContent value="ocr" className="flex-1 direction-vertical border divide-y">
+            {!file.data.ocr_vtt && <div className="size-full flex items-center justify-center">
+              <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-cyan-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+            </div>}
             {file.data.ocr_vtt?.map((item, index) => <div className="relative h-20" key={index}>
               <div className="flex h-full items-center">
                 <div className="flex h-full w-7 flex-col items-center justify-between border-r border-gray-200 py-3 text-base text-black/40">
